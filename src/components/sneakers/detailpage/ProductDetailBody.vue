5f123a5806f7c8cc944751c494bbafeb9bc1b0dd
isaac <isaacshin92@gmail.com>
2023-01-08 22:55:07 +0900

    favicon

--- src/components/sneakers/detailpage/ProductDetailBody.vue
+++ src/components/sneakers/detailpage/ProductDetailBody.vue
@@ -1,93 +1,108 @@
 <template>
-  <div class="text-center h-100 w-100" style="background-color:white;">
-    <div class="row h-100 border-bottom" v-show="kick">
-    <div>
-      <div class="mt-2">
-      <v-btn small absolute fab right style="margin-right: 8px;" @click="like_btn(kick?.id, index)" v-if="check_like_user(kick.like_users)">
-          <font-awesome-icon icon="fa-solid fa-heart" />
-        </v-btn>
-        <v-btn small absolute fab right style="margin-right: 8px;" @click="like_btn(kick?.id, index)" v-else>
-          <font-awesome-icon icon="fa-regular fa-heart" />                 
-        </v-btn>
+  <v-row align="center" align-content="space-around" no-gutters>
+    <div class="text-center">
+      <v-btn 
+        rounded color="#D6E4E5" 
+        top
+        style="margin-top:10px"
+        max-width="500px"          
+        v-if="img_check || 
+              kick.brand == null || 
+              kick.colorway == null ||
+              kick.releaseDate == '1900-01-01'||
+              kick.releaseDate == null ||
+              kick.retailPrice == null ||
+              kick.category == ''
+              " 
+        @click="add_info(kick?.id)" 
+      > 
+        사진 및 정보 등록하기 <br/>
+        [최대 +1000 points 적립]
+      </v-btn>
       </div>
-      <v-menu offset-y>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn v-bind="attrs" v-on="on" icon x-large style="float: left;">
-            <font-awesome-icon icon="fa-solid fa-ellipsis-vertical" />
+    <div class="mt-2 mb-2">
+      <v-btn small absolute fab right style="margin-top:3px; margin-right:12px;" @click="like_btn(kick?.id, index)" v-if="check_like_user(kick.like_users)">
+          <font-awesome-icon icon="fa-solid fa-heart" />
+      </v-btn>
+      <v-btn small absolute fab right style="margin-right:12px; margin-top:3px;" @click="like_btn(kick?.id, index)" v-else>
+        <font-awesome-icon icon="fa-regular fa-heart" />                 
+      </v-btn>
+    <v-menu offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn v-bind="attrs" v-on="on" icon x-large style="float: left;">
+          <font-awesome-icon icon="fa-solid fa-ellipsis-vertical" />
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item>공유하기</v-list-item>
+        <v-list-item>수정제안</v-list-item>
+      </v-list>
+    </v-menu>
+    </div>
+    <v-col sm="12" md="6" xl="6" class=" d-xl-none d-lg-none">
+      <v-carousel
+      cycle
+      height="600"
+      show-arrows-on-hover
+      id="img_caraousel" 
+      interval="6000" 
+      touch="true" 
+      hide-delimiter-background 
+      hide-delimiters
+    >
+        <template v-slot:prev="{ on, attrs }">
+          <v-btn
+            v-bind="attrs"
+            v-on="on"
+            icon
+            fab small
+          >
+          <span class="material-symbols-outlined">chevron_left</span>
           </v-btn>
         </template>
-        <v-list>
-          <v-list-item>공유하기</v-list-item>
-          <v-list-item>수정제안</v-list-item>
-        </v-list>
-      </v-menu>
-    </div>
-      <v-row justify="center" class="">
-        
-        <v-img :src="img_url" contain lazy-src="" alt="main_img"  class="mt-15 img-row" max-width="500" max-height="300" >
-        <v-btn style="margin-top: 7rem;"  rounded  color="primary" dark v-if="kick?.local_imageUrl == 'http://localhost:8000/media/images/defaultImg.png'" > 
-          사진 등록하기 <br/>
-          [+ 100 points 적립]
-        </v-btn>
-
-        <v-btn class="original_btn" elevation="10" rounded shaped style="margin-top: 7rem;" @click="original_size_img(kick?.local_imageUrl, k?.name)">크게보기</v-btn>
-
-
-        <template v-slot:placeholder>
-        <v-row
-          class="fill-height ma-0"
-          align="center"
-          justify="center"
+        <template v-slot:next="{ on, attrs }">
+          <v-btn
+            v-bind="attrs"
+            v-on="on"
+            icon fab small
+          >
+          <span class="material-symbols-outlined">chevron_right</span>
+          </v-btn>
+        </template>
+        <v-carousel-item 
+          v-for="(img, i) in kick.productImg"
+          :key="i"
         >
-          <v-progress-circular
-            indeterminate
-            color="grey lighten-5"
-          ></v-progress-circular>
-        </v-row>
-      </template>
-        </v-img>
+          <v-sheet height="100%">
+            <v-row
+              class="fill-height"
+              align="center"
+              justify="center"
+            >
+            <v-img :src="img.img_url" contain width="650" height="700" >
+              <div class="text-center">
+              <v-btn class="original_btn" elevation="10" rounded shaped style="margin-top: 15rem;" @click="original_size_img(img.img_url, kick.name)">크게보기</v-btn>
+              </div>
+            </v-img>
+            </v-row>
+          </v-sheet>
+        </v-carousel-item>
+      </v-carousel>
+    </v-col>
+    <v-col sm="12" md="6" xl="6" class="d-none d-lg-flex  d-xl-flex">
+    <v-card v-for="(img, i) in kick.productImg" :key="i">
+      <v-img :src="img.img_url"></v-img>
+    </v-card>
+    </v-col>
+    <v-col class="mt-10" dense sm="12" md="6" xl="6">
+      <v-card>
 
-      <div class="col-md-6 col-sm-12 text-start mt-16 ml-16">
-        <p class="fw-normal" v-if="kick.brand">{{kick?.brand.includes('%20')? kick.brand.replaceAll('%20', ' ').toUpperCase() :kick.brand.toUpperCase()}}</p>
-        <p class="fw-normal" v-else>Brand</p>
-        <h4 class="fw-bold">{{kick?.name}}</h4>
-        <div class='v-line'></div>
-            <v-rating
-            :value="this.rating"
-            dense
-            readonly
-            half-icon
-            color="orange"
-            background-color="orange"
-            half-increments
-            class="mr-2"
-          ></v-rating>
-          <!-- <star-rating :star-size="20" active-color="#000040" :read-only="true" :show-rating="false" dense v-model="this.rating"></star-rating> -->
-          <!-- TODO: disscusion  -->
-        <p>
-          <span class="" style="margin-top: 13px;">{{ get_review_cnt }} discussions</span> 
-        </p>
-        
-        <p>
-        <span class="fw-bold">발매일 : </span>  
-        <span> {{kick?.releaseDate}}</span>
-        </p>
-        <p>
-        <span class="fw-bold">SKU : </span>  
-        <span> {{kick?.sku}}</span>
-        </p>
-        <p>
-        <span class="fw-bold">ColorWay : </span>  
-        <span> {{kick?.colorway}}</span>
-        </p>
-        <p>
-        <span class="fw-bold">발매금액 : </span>  
-        <span> USD: ${{get_dollor}} / KRW: 약 {{get_krw.toLocaleString()}}원</span>
-        </p>
-      </div>  
-    </v-row>
-  </div>
-  </div>
+      </v-card>
+    </v-col>
+  </v-row>
+
+
+
 </template>
 
 <script>
@@ -99,11 +114,10 @@ export default {
   components: {
     // StarRating,
   },
-  props: {
-    kick : null,
-  },
+  
   data(){
     return {
+      kick : null,
       rating: 0,
       like_users: [],
     }
@@ -111,7 +125,7 @@ export default {
   methods: {
     get_avg_rating(){
       let avg_rating = 0
-      if(this.$props.kick){
+      if(this.kick){
         if(this.kick.reviews.length > 0){
           for(const r of this.kick.reviews) {
             avg_rating += r.rating
@@ -187,14 +201,24 @@ export default {
       }
     },
     set_like_user(){
-      if(this.$props.kick){
+      if(this.kick){
         console.log("chec")
-        this.like_users = this.$props.kick.like_users
+        this.like_users = this.kick.like_users
       }
     },
     original_size_img(url, name){
       const options = 'width=850, height=850, menubar=no, toolbar=no'
       window.open(url, name,options);
+    },
+        /**이미지 리스트 중 하나라도 미등록 상태일 경우 정보 등록 버튼을 노출시킨다. return boolean */
+    img_check(){
+      let result = false
+      for(let img of this.kick.productImg){
+        if(img.img_url =="http://localhost:8000/media/images/defaultImg.png")
+          result = true
+          break;
+      }
+    return result
     }
   }, // end of methods 
   computed : {
@@ -202,14 +226,14 @@ export default {
       return this.kick?.local_imageUrl
     },
     get_review_cnt() {
-      if(this.$props.kick){
+      if(this.kick){
         return this.kick.reviews.length
       }
       return 0;
     },
     get_krw(){
       const rate = 1302.58
-      if(this.$props.kick.retailPrice){
+      if(this.kick.retailPrice){
         const krw_price = ((this.kick.retailPrice)* 0.01 * rate)
         return Math.ceil(krw_price)
       }
@@ -237,6 +261,7 @@ export default {
       }
 
     },
+
   },
   watch: {
     kick(){
@@ -244,17 +269,33 @@ export default {
       this.set_like_user();
     }
   },
+    created(){
+    const product_id = this.$route.params.id;
+    axios({
+      method: 'GET',
+      url: 'http://127.0.0.1:8000/kicks/sneaker/' + product_id,
+    }).then(res=> {
+      console.log('detail res: ', res)
+      this.kick = res.data
+      this.product_id = res.data.id
+
+    }).catch(err => {
+      console.log('detail err :', err)
+      this.$router.push('/404')
+    })
+  },
   mounted() {
   }
 }
 </script>
 
 <style scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Pathway+Gothic+One&display=swap');
   .v-line {
     border-top : thin solid #c6c8c9;
     /* margin-left: 1rem; */
     height : 1px;
-    width: 634px;
+    width: 1234px;
     margin-bottom: 3.5px;
   }
 
@@ -266,7 +307,20 @@ export default {
     display: none;
   }
 
-  .img-row:hover .original_btn {
+  #img_caraousel{
+    box-shadow:10px 20px 25px #c6c8c9;
+    border-radius: 21px;
+  }
+
+
+  #img_caraousel:hover .original_btn {
     display: inline;
+  }
+  #korean_name{
+    font-family: 'Pathway Gothic One', sans-serif;
+  }
+
+  .product_name{
+    font-family: 'Pathway Gothic One', sans-serif;
   }
 </style>
--- src/views/SneakerDetailView.vue
+++ src/views/SneakerDetailView.vue
@@ -1,27 +1,23 @@
 <template>
-<v-app id="inspire">
-    <v-main class="sneaker-main h-100">
-      <v-container fluid class="w-100" >
+    <v-row no-gutters>
           <v-btn 
-            style="margin-top: -55px;"
             top
             left
-            elevation="10"
+            elevation="5"
+            max-width="68"
             @click="$router.go(-1)"
           >
           뒤로가기
           </v-btn>
-          <!-- <productDetail :kick=kick></productDetail> -->
-          <productDetailBody :kick=kick></productDetailBody>
+          <productDetailBody></productDetailBody>
           <reviewList :product_id=product_id></reviewList>
-      </v-container>
-    </v-main>
-  </v-app>
+    </v-row>
+
   
 </template>
 
 <script>
-import axios from 'axios'
+// import axios from 'axios'
 
 import productDetailBody from '@/components/sneakers/detailpage/ProductDetailBody.vue'
 import reviewList from '@/components/sneakers/detailpage/ReviewList.vue'
@@ -40,21 +36,7 @@ data() {
     current_page_content : null
   }
 },
-  created(){
-    const product_id = this.$route.params.id;
-    axios({
-      method: 'GET',
-      url: 'http://127.0.0.1:8000/kicks/sneaker/' + product_id,
-    }).then(res=> {
-      console.log('detail res: ', res)
-      this.kick = res.data
-      this.product_id = res.data.id
 
-    }).catch(err => {
-      console.log('detail err :', err)
-      this.$router.push('/404')
-    })
-  },
   methods: {
     
 
