6af3cfcf5eb041b8ebda6aabf75ef5a93feea671
신기욱 전임교수 <isaacshin92@gmail.com>
2023-03-23 21:27:50 +0900

    minor fixes

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,67 @@
 <template>
-  <v-app >
-    <!-- <v-navigation-drawer app>
-    </v-navigation-drawer>
-    <v-app-bar app>
-    </v-app-bar> -->
-    <v-app-bar
-      color="white"
-      flat
-      width="100%"
-      height="38%"
-      
-    >
+  <v-app>
+    <v-app-bar color="white" flat width="100%" height="38%">
       <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
       <v-toolbar-title class="banner_title accent-1 fs-2" @click="toHome">
-        KickIn 
+        KickIn
         <span class="material-symbols-outlined">barefoot</span>
-        </v-toolbar-title>
-      <template v-slot:extension >
-        <v-tabs  class="d-none d-sm-flex"  v-model="group"  hide-slider inactive-color="grey" v-if="isLogin" >
-          <v-tab  :ripple="false" >Profile</v-tab>
-
+      </v-toolbar-title>
+      <template v-slot:extension>
+        <v-tabs
+          class="d-none d-sm-flex"
+          v-model="group"
+          hide-slider
+          inactive-color="grey"
+          v-if="isLogin"
+        >
+          <v-tab :ripple="false">Profile</v-tab>
         </v-tabs>
-        <v-tabs class="d-none d-sm-flex"  v-model="group"  hide-slider inactive-color="black" v-else>
-          <v-tab  :ripple="false" @click="toLogin">
+        <v-tabs
+          class="d-none d-sm-flex"
+          v-model="group"
+          hide-slider
+          inactive-color="black"
+          v-else
+        >
+          <v-tab :ripple="false" @click="toLogin">
             <!-- <v-btn  class="fw-2" outlined elevation="1" > -->
             Login
             <!-- </v-btn> -->
-            </v-tab >
-          <v-tab  :ripple="false" @click="toAgreement">
+          </v-tab>
+          <v-tab :ripple="false" @click="toAgreement">
             <!-- <v-btn class="fw-2" outlined elevation="1"> -->
             Sign Up
             <!-- </v-btn> -->
-            </v-tab>
+          </v-tab>
         </v-tabs>
-        <v-tabs class="d-none d-sm-flex"   v-model="menu_group"  hide-slider inactive-color="grey" style="flex-direction: row-reverse; border-bottom:1px solid black;">
-          <v-tab  :ripple="false" color="black" @click="toHome">Home</v-tab>
+        <v-tabs
+          class="d-none d-sm-flex"
+          v-model="menu_group"
+          hide-slider
+          inactive-color="grey"
+          style="flex-direction: row-reverse; border-bottom: 1px solid black"
+        >
+          <v-tab :ripple="false" color="black" @click="toHome">Home</v-tab>
           <v-tab :ripple="false" color="black" @click="toSneakers">Items</v-tab>
-          <v-tab :ripple="false" color="black" @click="toCulture">Culture</v-tab>
+          <v-tab :ripple="false" color="black" @click="toCulture"
+            >Culture</v-tab
+          >
           <v-tab :ripple="false">Raffle</v-tab>
           <v-tab :ripple="false" @click="toCalendar">Calendar</v-tab>
           <v-tab :ripple="false" @click="toAuction">Auction</v-tab>
         </v-tabs>
       </template>
-
     </v-app-bar>
-    <v-app-bar dense collapse color="white" flat max-width="400" shrink-on-scroll class="mt-5" style="flex-direction: row-reverse; border-bottom:1px solid black;">
+    <v-app-bar
+      dense
+      collapse
+      color="white"
+      flat
+      max-width="400"
+      shrink-on-scroll
+      class="mt-5"
+      style="flex-direction: row-reverse; border-bottom: 1px solid black"
+    >
       <v-text-field
         class=""
         label="Search Keyword"
@@ -57,16 +74,21 @@
       v-model="drawer"
       fixed
       temporary
-      mobile-breakpoint='5'
+      mobile-breakpoint="5"
       expand-on-hover
     >
       <v-list nav dense>
         <v-list-item-group>
-        <v-list-item v-if="this.$store.state.user_data.access_token" @click="toProfile">
-          <v-list-item-avatar>
-            <v-img :src="this.$store.state.user_data.profile_img"></v-img>
-          </v-list-item-avatar>
-            <v-list-item-title class="font-weight-black accent-4 text-h6">{{this.$store.state.user_data.nick_name}}</v-list-item-title>
+          <v-list-item
+            v-if="this.$store.state.user_data.access_token"
+            @click="toProfile"
+          >
+            <v-list-item-avatar>
+              <v-img :src="this.$store.state.user_data.profile_img"></v-img>
+            </v-list-item-avatar>
+            <v-list-item-title class="font-weight-black accent-4 text-h6">{{
+              this.$store.state.user_data.nick_name
+            }}</v-list-item-title>
           </v-list-item>
           <v-list-item v-else @click="toLogin">
             <v-list-item-icon>
@@ -107,7 +129,7 @@
             </v-list-item-icon>
             <v-list-item-title>Raffle</v-list-item-title>
           </v-list-item>
-        
+
           <v-list-item @click="toCalendar">
             <v-list-item-icon>
               <span class="material-symbols-outlined">calendar_month</span>
@@ -120,45 +142,36 @@
             </v-list-item-icon>
             <v-list-item-title>Auction</v-list-item-title>
           </v-list-item>
-
-
-
         </v-list-item-group>
       </v-list>
-          <v-btn v-if="this.$store.state.user_data.access_token" icon style="margin-top:30.3rem;" @click="signoutRequest">
-            <span class="material-symbols-outlined">logout</span>
-          </v-btn>
+      <v-btn
+        v-if="this.$store.state.user_data.access_token"
+        icon
+        style="margin-top: 30.3rem"
+        @click="signoutRequest"
+      >
+        <span class="material-symbols-outlined">logout</span>
+      </v-btn>
     </v-navigation-drawer>
     <v-main>
       <v-container fluid>
         <transition name="fade">
-          <loadingImg v-if="this.$store.state.isLoading"/>
-          <router-view :key="$route.fullPath" v-else/>
+          <loadingImg v-if="this.$store.state.isLoading" />
+          <router-view :key="$route.fullPath" v-else />
         </transition>
       </v-container>
     </v-main>
-  <v-footer
-      dark
-      padlessq
-    >
-      <v-card
-        class="flex"
-        flat
-        tile
-      >
+    <v-footer dark padlessq>
+      <v-card class="flex" flat tile>
         <v-card-title class="teal">
-          <strong class="subheading">Get connected with us on social networks!</strong>
+          <strong class="subheading"
+            >Get connected with us on social networks!</strong
+          >
 
           <v-spacer></v-spacer>
 
-          <v-btn
-            v-for="icon in icons"
-            :key="icon"
-            class="mx-4"
-            dark
-            icon
-          >
-            <v-icon size="24px">
+          <v-btn v-for="icon in icons" :key="icon" class="mx-4" dark icon>
+            <v-icon size="24px" @click="to_url(icon)">
               {{ icon }}
             </v-icon>
           </v-btn>
@@ -175,7 +188,7 @@
 <script>
 // import HeaderMenu from "@/components/layouts/LayoutHeader.vue";
 // import FooterMenu from "@/components/layouts/LayoutFooter.vue";
-import loadingImg from '@/components/common/loadingPage.vue'
+import loadingImg from "@/components/common/loadingPage.vue";
 export default {
   name: "App",
   components: {
@@ -189,12 +202,7 @@ export default {
     menu_group: null,
     group: null,
     keyword: null,
-    icons: [
-        'mdi-facebook',
-        'mdi-twitter',
-        'mdi-linkedin',
-        'mdi-instagram',
-      ],
+    icons: ["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"],
   }),
   mounted() {
     setTimeout(() => {
@@ -203,107 +211,118 @@ export default {
   },
   methods: {
     setLoading() {
-      console.log('App.Vue created')
-      this.isLoading = true
+      console.log("App.Vue created");
+      this.isLoading = true;
     },
-    toHome(){
+    toHome() {
       //TODO: 현재 위치한 페이지 볼드 처리 .
       // this.$store.dispatch('setLoading', true)
-        this.$router.push({name:'home'})
-        this.menu_group = 'home'
-      
+      this.$router.push({ name: "home" });
+      this.menu_group = "home";
     },
-    toSneakers(){
+    toSneakers() {
       // this.$store.dispatch('setLoading', true)
-      this.menu_group = 'items'
-      let today = new Date()
+      this.menu_group = "items";
+      let today = new Date();
       let year = today.getFullYear();
-      let month = ('0' + (today.getMonth() + 1)).slice(-2);
-      let day = ('0' + today.getDate()).slice(-2);
+      let month = ("0" + (today.getMonth() + 1)).slice(-2);
+      let day = ("0" + today.getDate()).slice(-2);
       const release = year + month + day;
-      console.log('toSneakers toSneakers', release)
-      this.$router.push({name:'sneakers', 
-                        query: {
-                          search: '',
-                          brand: '',
-                          release: '',
-                        }})
-      
+      console.log("toSneakers toSneakers", release);
+      this.$router.push({
+        name: "sneakers",
+        query: {
+          search: "",
+          brand: "",
+          release: "",
+        },
+      });
     },
-    toCalendar(){
-      this.$store.dispatch('setLoading').then(
-        this.$router.push({name:'calendar'})
-      )
+    toCalendar() {
+      this.$store
+        .dispatch("setLoading")
+        .then(this.$router.push({ name: "calendar" }));
     },
-    toLogin(){
-      this.group = 'login'
-      this.$store.dispatch('setLoading').then(
-        this.$router.push({name:'login'}) 
-      )
+    toLogin() {
+      this.group = "login";
+      this.$store
+        .dispatch("setLoading")
+        .then(this.$router.push({ name: "login" }));
     },
-    signoutRequest(){
-      this.$store.dispatch("signoutRequest")
-      .then(() => this.$router.push({name:'login'}))
-      },
-    toCulture(){
-      this.$router.push({name:'culture'})
+    signoutRequest() {
+      this.$store
+        .dispatch("signoutRequest")
+        .then(() => this.$router.push({ name: "login" }));
     },
-    toAuction(){
-      this.$router.push({name:'auction'})
+    toCulture() {
+      this.$router.push({ name: "culture" });
     },
-    toProfile(){
+    toAuction() {
+      this.$router.push({ name: "auction" });
+    },
+    toProfile() {
       // this.$router.push({name:'profile'})
     },
-    searchKeyword(){
-      console.log('search', this.keyword )
-      this.keyword = '';
+    searchKeyword() {
+      console.log("search", this.keyword);
+      this.keyword = "";
     },
     toAgreement() {
-      this.$router.push({path: '/agreement'})
-      this.group='agreement'
+      this.$router.push({ path: "/agreement" });
+      this.group = "agreement";
+    },
+    to_url(icon) {
+      if (icon == "mdi-facebook") {
+        window.open("https://www.facebook.com/");
+      } else if (icon == "mdi-twitter") {
+        window.open("https://twitter.com/");
+      } else if (icon == "mdi-linkedin") {
+        window.open("https://www.linkedin.com/");
+      } else if (icon == "mdi-instagram") {
+        window.open("https://www.instagram.com/kickin.co.kr/");
+      }
     },
   },
-  computed:{
-    is_loading(){
-      console.log(this.$state.isLoading)
+  computed: {
+    is_loading() {
+      console.log(this.$state.isLoading);
       return true;
     },
-    isLogin(){
-      return this.$store.state.user_data.access_token
+    isLogin() {
+      return this.$store.state.user_data.access_token;
     },
   },
   created() {
     // console.log(this.$store.state.user_data.profile_img)
     // console.log()
-    this.setLoading()
+    this.setLoading();
   },
 };
 </script>
 <style scoped>
-.fade-enter-active, .fade-leave-active {
+.fade-enter-active,
+.fade-leave-active {
   transition-property: opacity;
-  transition-duration: .25s;
+  transition-duration: 0.25s;
 }
 
 .fade-enter-active {
-  transition-delay: .25s;
+  transition-delay: 0.25s;
 }
 
-.fade-enter, .fade-leave-active {
-  opacity: 0
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
 }
 
 .nav-mouse-over {
   cursor: pointer;
 }
-.banner_title{
-  font-family: 'Lobster';
-  font-family: 'Pathway Gothic One', sans-serif;
+.banner_title {
+  font-family: "Lobster";
+  font-family: "Pathway Gothic One", sans-serif;
   font-weight: 900;
   cursor: pointer;
-  
-
 }
-  
 </style>
 
